fbedf4d65b3ff6e867bc35212c27196a48d83091
vidyant Sharma <vidyantkumar5519@gmail.com>
2025-05-25 20:00:20 +0530

    env

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/vidyantTechnology', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vidyantTechnology', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
